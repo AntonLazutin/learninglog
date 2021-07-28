918fbcac41335227087a16f3d438f05067186127
Anton Lazutin <chuvak209@gmail.com>
2021-07-29 00:57:37 +0300

    Learning log without authorisation and edit

---
+++ learning_logs/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm, EntryForm
+
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('topics')
+
+    return render(request, 'learning_logs/new_topic.html', {'form': form})
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('topic', topic_id=topic_id)
+    context = {'form': form, 'topic': topic}
+    return render(request, 'learning_logs/new_entry.html', context)
